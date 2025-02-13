83c48e5fbb37958d96739e00d7514f06f077c3d7
Lixin Nie <lixinnie@terpmail.umd.edu>
2025-02-13 16:32:48 -0500

    yes

--- Imparative_Ocaml.ml
+++ Imparative_Ocaml.ml
@@ -11,5 +11,11 @@ type coin = Heads|Tails (*This defines a type, similiar to struct in c or class 
 let count = ref 0 (*ref makes a reference to the piece of data you give to it, in this case count is pointing towards the reference of zero*)
 
 let new_num () = let res = !count in (*the '!' derefencces similiar to * in C !count dereferences the value stored in the reference*)
-                 count := !count +1  (*':=' takes in a reference and updates the value stored within the reference*)
+                 count := !count +1;  (*':=' takes in a reference and updates the value stored within the reference*)
+                                      (*notice the semicolon, it is used to seperate the instructions*)
                  res 
+
+
+
+(**)
+let x = print_string "hello\n" (*In this case x is bounded the the unit, hello is printed as a side effect of the function*)
